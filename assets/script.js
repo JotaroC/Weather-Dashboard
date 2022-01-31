013057c591b0e6d2487d6adae013854c8900563d
Alex C <cxz980314@gmail.com>
2022-01-30 23:27:20 -0500

    improve the UV color changing algorithm

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,6 @@
 let searchButtonEl =  $(".btn");
 let APIkey = "c33b2973c6711cf115abe047468b6b6e";
-var today = moment().format('L');
-let cityName = "oakville"
+let today = moment().format('L');
 let cityList = [];
 
 //current time and date
@@ -11,16 +10,16 @@ let dateTime = moment().format('YYYY-MM-DD HH:MM:SS')
 // WHEN I open the weather dashboard
 // THEN I am presented with the last searched city forecast
 $(document).ready(function() {
-    let searchHistoryArr = JSON.parse(localStorage.getItem("city"));
+    let searchHistory = JSON.parse(localStorage.getItem("city"));
 
-    if (searchHistoryArr !== null) {
-        let lastSearchedCity = searchHistoryArr[searchHistoryArr.length - 1];
-        // currentCondition(lastSearchedCity);
+    if (searchHistory !== null) {
+        let lastSearchedCity = searchHistory[searchHistory.length - 1];
+        currentWeather(lastSearchedCity);
         console.log(`Last searched city: ${lastSearchedCity}`);
     }
 });
 
-function currentCondition(name) {
+function currentWeather(name) {
     let queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${name}&units=metric&appid=${APIkey}`;
     $.ajax({
         url: queryURL,
@@ -54,33 +53,30 @@ function currentCondition(name) {
         }).then(function(uvResponse) {
             console.log("UVResponse");
             console.log(uvResponse);
+
             let uvIndex = uvResponse.value;
             let currentUV = $(`<p>UV Index: 
-            <span id="uvIndexColor" class="px-2 py-2 rounded">${uvIndex}</span>
+            <span id="uvColor" class="px-2 py-1 rounded">${uvIndex}</span>
             </p>`);
 
             $("#today-weather").append(currentUV);
-            futureCondition(lat, lon);
+            futureWeather(lat, lon);
 
             // WHEN I view the UV index
             // THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
             // 0-2 green#3EA72D, 3-5 yellow#FFF300, 6-7 orange#F18B00, 8-10 red#E53210, 11+violet#B567A4
-            if (uvIndex >= 0 && uvIndex <= 2) {
-                $("#uvIndexColor").css("background-color", "#3EA72D").css("color", "white");
-            } else if (uvIndex >= 3 && uvIndex <= 5) {
-                $("#uvIndexColor").css("background-color", "#FFF300");
-            } else if (uvIndex >= 6 && uvIndex <= 7) {
-                $("#uvIndexColor").css("background-color", "#F18B00");
-            } else if (uvIndex >= 8 && uvIndex <= 10) {
-                $("#uvIndexColor").css("background-color", "#E53210").css("color", "white");
+            if (uvIndex >= 0 && uvIndex <= 4) {
+                $("#uvColor").addClass("badge bg-success")
+            } else if (uvIndex >= 5 && uvIndex <= 8) {
+                $("#uvColor").addClass("badge bg-warning text-dark");
             } else {
-                $("#uvIndexColor").css("background-color", "#B567A4").css("color", "white"); 
+                $("#uvColor").addClass("badge bg-danger")
             }; 
         })
     })
 }
 
-function futureCondition(lat,lon) {
+function futureWeather(lat,lon) {
     let futureURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude={current,minutely,hourly,alerts}&units=metric&appid=${APIkey}`;
 
     $.ajax({
@@ -95,7 +91,7 @@ function futureCondition(lat,lon) {
         $('#boxes').empty();
 
         for (let i = 1; i < 6; i++) {
-            var cityInfo = {
+            let weatherInfo = {
                 date: futureResponse.daily[i].dt,
                 icon: futureResponse.daily[i].weather[0].icon,
                 temp: futureResponse.daily[i].temp.day,
@@ -103,21 +99,18 @@ function futureCondition(lat,lon) {
                 humidity: futureResponse.daily[i].humidity
             };
 
-            let currDate = moment.unix(cityInfo.date).format("MM/DD/YYYY");
-            let iconURL = `<img src="https://openweathermap.org/img/w/${cityInfo.icon}.png" alt="${futureResponse.daily[i].weather[0].main}" />`;
+            let date = moment.unix(weatherInfo.date).format("MM/DD/YYYY");
+            let iconURL = `<img src="https://openweathermap.org/img/w/${weatherInfo.icon}.png"/>`;
 
             // displays the date
-            // an icon representation of weather conditions
-            // the temperature
-            // the humidity
             let futureCard = $(`
                     <div class="card pl-3 pt-3 mx-3 bg-primary text-light" style="width: 12rem;>
                         <div class="card-body">
-                            <h5>${currDate}</h5>
+                            <h5>${date}</h5>
                             <p>${iconURL}</p>
-                            <p>Temp: ${cityInfo.temp} °C</p>
-                            <p>Wind: ${cityInfo.wind} m/s</p>
-                            <p>Humidity: ${cityInfo.humidity}\%</p>
+                            <p>Temp: ${weatherInfo.temp} °C</p>
+                            <p>Wind: ${weatherInfo.wind} m/s</p>
+                            <p>Humidity: ${weatherInfo.humidity}\%</p>
                         </div>
                     </div>`);
 
@@ -130,12 +123,10 @@ $(".btn").on("click", function(event) {
     event.preventDefault();
 
     let cityName = $("#search-city").val().trim();
-    currentCondition(cityName);
+    currentWeather(cityName);
     if (!cityList.includes(cityName)) {
         cityList.push(cityName);
-        let searchedCity = $(`
-            <li class="list-group-item">${cityName}</li>
-            `);
+        let searchedCity = $(`<li class="list-group-item">${cityName}</li>`);
         $(".list-group").append(searchedCity);
     };
 
@@ -145,5 +136,5 @@ $(".btn").on("click", function(event) {
 
 $(document).on("click", ".list-group-item", function() {
     let city = $(this).text();
-    currentCondition(city);
+    currentWeather(city);
 })
